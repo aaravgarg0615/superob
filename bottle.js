5fd87cbde190541099e0bd0a87d9f37232cc9d90
aaravgarg0615 <79427022+aaravgarg0615@users.noreply.github.com>
2021-08-25 11:42:42 -0400

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,57 @@
+img = "";
+status = "";
+objects = [];
+r = (0);
+g = (0);
+b = (205);
+function preload() {
+    img = loadImage('bottles.jpg');
+
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("coccossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : detecting objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded cocossd is loaded")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Objects Detected are :" + objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function change(){
+    r = random(255);
+    g = random(255);
+    b = random(255);
+}
+function back(){
+    window.location.replace("index.html");
+}
+
---
+++ main.js
@@ -0,0 +1,26 @@
+function bed()
+{
+    window.window.location.replace("bed.html");
+}
+
+function tv()
+{
+    window.window.location.replace("tv.html");
+}
+function bottle()
+{
+    window.window.location.replace("bottle.html");
+}
+function desk()
+{
+    window.window.location.replace("desk.html");
+}
+function vid()
+{
+    window.window.location.replace("vid.html");
+}
+
+function fruit()
+{
+    window.window.location.replace("fruit.html");
+}
